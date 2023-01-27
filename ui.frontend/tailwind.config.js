7b6868a0e908f45b1bf23bfc058f414a744193ad
Connor Wade <connor@hoodoo.digital>
2023-01-27 13:53:51 -0700

    finished first stage of experiment

--- ui.frontend/tailwind.config.js
+++ ui.frontend/tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
 	],
 	theme: {
 		extend: {},
+		font: {
+			'font-family': '"Helvetica Neue", Helvetica, Arial, sans-serif;',
+			'font-size': '16px',
+			'font-height': '1.5',
+		},
+		colors: {
+			'foreground': '#202020',
+			'background': '#ECECEC',
+			'link': '#2020E0',
+		},
 	},
 	plugins: [],
+	safelist: [
+		'p-1',
+		'p-2',
+		'p-3',
+		'p-4',
+		'p-5'
+	]
 };
